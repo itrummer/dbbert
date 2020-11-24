ad9bea2a27762d09246c818f4109ebf8970ec820
itrummer <immanuel.trummer@gmail.com>
2020-11-23 19:02:58 -0500

    added TPC-H evaluation for PG

--- Analysis.py
+++ Analysis.py
@@ -30,37 +30,51 @@ def alternative_vals(val):
             str(int(int(nr) * 5)) + unit]
     return alt_vals + [val]
 
-# Prepare tuning and perform NLP analysis
-"""
-print("About to create Postgres tuning config")
-pg_config = Configurations.TuningConfig(
-    '/etc/postgresql/10/main/postgresql.conf')
-"""
+# Prepare tuning for MySQL
 print("Reading default MySQL configuration file")
-config = configparser.ConfigParser()
-config.read('mysql/all_tunable_config.cnf')
-print("Create MySQL evaluation object")
-tpch_eval = Evaluate.MySQLeval(config)
+ms_config = configparser.ConfigParser()
+ms_config.read('mysql/all_tunable_config.cnf')
+print("Creating MySQL evaluation object")
+ms_eval = Evaluate.MySQLeval(ms_config)
+
+# Prepare tuning for Postgres
+print("Reading Postgres configuration file")
+pg_config = configparser.ConfigParser()
+with open('/home/ubuntu/liter2src/configs/pg_default.conf') as stream:
+    pg_config.read_string("[dummysection]\n" + stream.read())
+print("Creating Postgres evaluation object")
+pg_eval = Evaluate.PostgresEval(pg_config)
+
+# Select database system to tune
+config = pg_config
+db_eval = pg_eval
 
 # Initialize file for result output
-f = open("mysqlTpch.txt", "w")
+f = open("tpch.txt", "w")
 f.write('param\tval\tquery\terror\tmillis')
 f.flush()
-# Iterate over parameters
-for param in config['mysqld-5.7']:
-    # Extract original parameter value
-    val = config['mysqld-5.7'][param]
-    print(f"Parameter {param} with value {val}", flush=True)
-    # Iterate over alternative values
-    alt_vals = alternative_vals(val)
-    print(f"Alternative values: {alt_vals}", flush=True)
-    if len(alt_vals) > 1:
-        for alt_val in alt_vals:
-            config['mysqld-5.7'][param] = alt_val
-            print(f'Alternative {alt_val} to {val}', flush=True)
-            error, times = tpch_eval.tpch_eval()
-            for q in range(22):
-                f.write(f'{param}\t{alt_val}\t{q+1}' 
-                        f'\t{error}\t{times[q]}\n')
-                f.flush()
+
+# Iterate over parameter sections
+for section in config.sections():
+    # Iterate over parameters in section
+    for param in config[section]:
+        # Extract original parameter value
+        print(f'Param: {param}', flush=True)
+        val = config[section][param]
+        print(f"Parameter {param} with value {val}", flush=True)
+        # Iterate over alternative values
+        alt_vals = alternative_vals(val)
+        print(f"Alternative values: {alt_vals}", flush=True)
+        if len(alt_vals) > 1:
+            for alt_val in alt_vals:
+                config[section][param] = alt_val
+                print(f'Alternative {alt_val} to {val}', 
+                      flush=True)
+                error, times = db_eval.tpch_eval()
+                for q in range(22):
+                    f.write(f'{param}\t{alt_val}\t{q+1}' 
+                            f'\t{error}\t{times[q]}\n')
+                    f.flush()
+
+# Close benchmark output file
 f.close()
--- Evaluate.py
+++ Evaluate.py
@@ -4,6 +4,7 @@ Created on Nov 16, 2020
 @author: immanueltrummer
 '''
 import os
+import psycopg2
 import time
 
 class MySQLeval:
@@ -56,6 +57,68 @@ class MySQLeval:
                         f'< queries/ms/{q}.sql')
                     print(f"Executed with code {r_code}", 
                           flush=True)
+                    end_ms = time.time() * 1000.0
+                    times[q-1] = end_ms - start_ms
+                error = False
+        except Exception as e:
+            error = True
+            print(f'Exception: {e}')
+        return error, times
+    
+class PostgresEval():
+    """ Benchmark Postgres database """
+    def __init__(self, config):
+        self.config = config
+        
+    def change_config(self):
+        # Stop Postgres server
+        r_code = os.system('sudo pg_ctlcluster 10 main stop')
+        print(f'Stopped server with code {r_code}')
+        # Override temporary configuration file
+        r_code = os.system('sudo rm tmpConfig')
+        print(f'removed configuration file with return {r_code}')
+        with open('tmpConfig', 'w') as config_file:
+            for p in self.config['dummysection']:
+                config_file.write(
+                    f"{p} = {self.config['dummysection'][p]}\n")
+            config_file.flush()
+        # Copy temporary configuration file
+        r_code = os.system('sudo cp --remove-destination ' \
+            'tmpConfig /etc/postgresql/10/main/postgresql.conf')
+        print(f'copied configuration file with return {r_code}')
+        # Restart Postgres server
+        r_code = os.system('sudo pg_ctlcluster 10 main start')
+        print(f'Started server with code {r_code}')
+        time.sleep(5)
+        return r_code != 0
+    
+    def exec_sql(self, query, dbname, dbuser, dbpassword):
+        """ Runs given query on given database """
+        connection = psycopg2.connect(database = dbname, 
+                user = dbuser, password = dbpassword,
+                host = "localhost")
+        connection.autocommit = True
+        cursor = connection.cursor()
+        cursor.execute(query)
+        connection.close()
+        
+    def tpch_eval(self):
+        """ Evaluate current configuration with TPC-H """
+        # Change configuration and iterate over TPC-H queries
+        error = False
+        times = [-1 for _ in range(22)]
+        try:
+            error |= self.change_config()
+            print(f"Error status is {error}")
+            if not error:
+                print("About to run benchmark", flush=True)
+                error = True
+                for q in range(1, 23):
+                    start_ms = time.time() * 1000.0
+                    with open(f'queries/pg/{q}.sql') as q_file:
+                        query = q_file.read()
+                        self.exec_sql(query, 'tpchs1', 
+                                      'postgres', 'postgres')
                     end_ms = time.time() * 1000.0
                     times[q-1] = end_ms - start_ms
                 error = False
