a263a1d414b05f6dee478c420a949d43dd136007
itrummer <immanuel.trummer@gmail.com>
2021-05-09 13:34:43 -0400

    Switched from command line parameters to reading configuration files

--- src/training/unsupervised.py
+++ src/training/unsupervised.py
@@ -10,6 +10,8 @@ from all.experiments import run_experiment
 from all.presets.classic_control import dqn
 from argparse import ArgumentParser
 from benchmark.evaluate import OLAP
+from configparser import ConfigParser
+from dbms.mysql import MySQLconfig
 from dbms.postgres import PgConfig
 from doc.collection import DocCollection
 from environment.multi_doc import MultiDocTuning
@@ -17,68 +19,66 @@ from models.bert_tuning import BertFineTuning
 from parameters.util import read_numerical
 from search.objectives import Objective
 
-# Parse command line arguments
-parser = ArgumentParser(description='Unsupervised training for NLU of DB tuning documents')
-parser.add_argument('device', type=str, help='"cuda" or "cpu"')
-parser.add_argument('docs', type=str, help='Path to file with tuning documents')
-parser.add_argument('queries', type=str, help='Path to file containing SQL queries')
-parser.add_argument('logging', type=str, help='Path to file for benchmark logging')
-parser.add_argument('user', type=str, help='User name for database access')
-parser.add_argument('db', type=str, help='Name of database for tuning')
-parser.add_argument('password', type=str, help='Password for database access')
-parser.add_argument('parameters', type=str, help='Path to file with parameters')
-parser.add_argument('data', type=str, help='Path to Postgres data directory')
-parser.add_argument('restart', type=str, help='Command for restarting DBMS')
-parser.add_argument('memory', type=int, help='Main memory', default=2000000)
-parser.add_argument('disk', type=int, help='Disk space', default=2000000)
-parser.add_argument('cores', type=int, help='Number of cores', default=8)
-parser.add_argument('nrhints', type=int, help='Number of hints per episode', default=1)
-parser.add_argument('nrevals', type=int, help='Number of evaluations per episode', default=1)
-parser.add_argument('nrframes', type=int, help='How many frames to execute', default=10000)
-parser.add_argument('sperf', type=float, help='Performance reward scaling', default=0.01)
-parser.add_argument('epsilon', type=float, help='Initial exploration epsilon', default=1)
-parser.add_argument('imodel', type=str, default='bert-base-cased',
-                    help='Name or path to read initial model')
-parser.add_argument('omodel', type=str, help='Path to write output model')
-args = parser.parse_args()
+# Read configuration referenced as command line parameters
+arg_parser = ArgumentParser(description='DB-BERT: Train NLU for DB tuning documents')
+arg_parser.add_argument('cpath', type=str, help='Path to configuration file')
+args = arg_parser.parse_args()
+config = ConfigParser()
+config.read(args.cpath)
 
-device = args.device
-path_to_docs = args.docs
-path_to_queries = args.queries
-log_path = args.logging
-db_user = args.user
-db_name = args.db
-password = args.password
-path_to_conf = args.parameters
-path_to_data = args.data
-restart_cmd = args.restart
-nr_frames = args.nrframes
-input_model = args.imodel
-output_model = args.omodel
+device = config['LEARNING']['device'] # cuda or cpu
+input_model = config['LEARNING']['input'] # name or path to input model
+output_model = config['LEARNING']['output'] # path to output model
+nr_frames = config['LEARNING']['nr_frames'] # number of frames
+epsilon = config['LEARNING']['start_epsilon'] # start value for epsilon
+p_scaling = config['LEARNING']['performance_scaling'] # scaling factor for performance reward
+nr_evals = config['LEARNING']['nr_evaluations'] # number of evaluations per episode
+nr_hints = config['LEARNING']['nr_hints'] # number of hints per episode
+
+dbms_name = config['DATABASE']['dbms']
+db_user = config['DATABASE']['user']
+db_name = config['DATABASE']['name']
+password = config['DATABASE']['password']
+restart_cmd = config['DATABASE']['restart_cmd']
+bin_dir = config['DATABASE']['bin_dir']
+path_to_data = config['DATABASE']['data_dir']
+path_to_conf = config['DATABASE']['config']
+
+path_to_docs = config['BENCHMARK']['docs']
+path_to_queries = config['BENCHMARK']['queries']
+log_path = config['BENCHMARK']['logging']
+memory = config['BENCHMARK']['memory']
+disk = config['BENCHMARK']['disk']
+cores = config['BENCHMARK']['cores']
 
 # Initialize tuning documents
 docs = DocCollection(docs_path=path_to_docs, 
                      dbms=None, size_threshold=0)
 
-# Configure Postgres database system
-pg_params = read_numerical(path_to_conf)
-postgres = PgConfig(db=db_name, user=db_user, 
+# Configure database management system
+if dbms_name == 'pg':
+    print('Using Postgres database')
+    pg_params = read_numerical(path_to_conf)
+    dbms = PgConfig(db=db_name, user=db_user, 
                     password=password,
                     restart_cmd=restart_cmd,
-                    data_dir=path_to_data)
-postgres.reset_config()
-postgres.reconfigure()
+                    data_dir=path_to_data)    
+else:
+    print('Using MySQL database')
+    dbms = MySQLconfig(db_name, db_user, password, bin_dir)
 
-# Initialize benchmark
-bench = OLAP(postgres, path_to_queries)
+# Initialize benchmark and DBMS
+dbms.reset_config()
+dbms.reconfigure()
+bench = OLAP(dbms, path_to_queries)
 bench.reset(log_path)
 
 # Initialize environment
 unsupervised_env = MultiDocTuning(
-    docs=docs, dbms=postgres, benchmark=bench, 
-    hardware=[args.memory, args.disk, args.memory], 
-    hints_per_episode=args.nrhints, nr_evals=args.nrevals, 
-    scale_perf=args.sperf, objective=Objective.TIME)
+    docs=docs, dbms=dbms, benchmark=bench,
+    hardware=[memory, disk, memory],
+    hints_per_episode=nr_hints, nr_evals=nr_evals,
+    scale_perf=p_scaling, objective=Objective.TIME)
 unsupervised_env = GymEnvironment(unsupervised_env, device=device)
 
 # Initialize agents
@@ -90,7 +90,7 @@ agent = dqn(
 
 # Run experiments
 run_experiment(agents=agent, envs=unsupervised_env, 
-               frames=nr_frames, test_episodes=10)
+               frames=nr_frames, test_episodes=1)
 
 # Save final model
 model.model.save_pretrained(output_model)
