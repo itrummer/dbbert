b69187ed0c5b2632de6c3f8c8a79501dfeff2bd6
itrummer <immanuel.trummer@gmail.com>
2021-05-09 16:12:21 -0400

    Mask parameter names

--- src/environment/bert_tuning.py
+++ src/environment/bert_tuning.py
@@ -25,6 +25,15 @@ class TuningBertFine(DocTuning):
             low=0, high=100000, shape=(3, 5, 512,), dtype=np.int64)
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
 
+    def _mask(self, strings, param):
+        """ Mask occurrence of parameter in string array. 
+        
+        Args:
+            strings: array of strings
+            param: mask this parameter
+        """
+        return [s.replace(param, '[MASK]') for s in strings]
+
     def _observe(self):
         """ Generate observation for current decision and hint. """
         if self.hint_ctr < self.nr_hints:
@@ -48,6 +57,9 @@ class TuningBertFine(DocTuning):
         else:
             v_weights = ['not', 'slightly', 'quite', 'very', 'extremely']
             choices = [f'The hint on {param} is {weight} important.' for weight in v_weights]
+        # Mask parameter name (generalization to different DBMS)
+        passage_cps = self._mask(passage_cps, param)
+        choices = self.mask(choices, param)
         encoding = self.tokenizer(
             passage_cps, choices, return_tensors='pt', 
             padding='max_length', truncation=True, max_length=512)
